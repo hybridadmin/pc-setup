16a17a083e28b3bba385972e0abb712a093e7450
hybridadmin <46610714+hybridadmin@users.noreply.github.com>
2020-04-16 16:35:46 +0200

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -6,37 +6,33 @@
 # https://github.com/PoshCode/Pansies - 1.4.0 required
 # https://www.nickjames.ca/my-environment-my-powershell-profile/
 # http://joonro.github.io/blog/posts/powershell-customizations.html
-if(!$IsWindows){
-	$Global:IsWindows = [Runtime.InteropServices.RuntimeInformation]::IsOSPlatform( [Runtime.InteropServices.OSPlatform]::Windows )
-}
 
 Set-Variable -Scope Global -Name IsAdmin -Value $(New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 
-#if(Get-Module psgit -ListAvailable){ Import-Module psgit }
 if(Get-Module PowerLine -ListAvailable){
 	#	Import-module PowerLine
-		[PoshCode.Pansies.Entities]::ExtendedCharacters.Separator = $([char]0x2572)	
-		[System.Collections.Generic.List[ScriptBlock]]$global:Prompt = @(
-			{New-PromptText {Get-Elapsed -Format "{0:mm\:ss\.ff}"} -ErrorBackgroundColor DarkRed -ErrorForegroundColor White -ForegroundColor White -BackgroundColor DarkGray  },
-			{New-PromptText {Get-Date -f "HH:mm:ss"} -ForegroundColor Black -BackgroundColor Gray  },
-			{New-PromptText { "PS" } -ForegroundColor White -BackgroundColor 'DarkGray'},
-			{New-PromptText { $($MyInvocation.HistoryId) } -ForegroundColor White -BackgroundColor 'DarkBlue'},
-			{ if ($Global:IsAdmin) {New-PromptText {"wurzel"} -BackgroundColor DarkRed -ForegroundColor White}else {New-PromptText { [environment]::UserName } -BackgroundColor Gray -ForegroundColor Black} },
-			{New-PromptText { "$($Env:COMPUTERNAME.ToLower())" } -ForegroundColor DarkGray -BackgroundColor Yellow},
-			{New-PromptText { ((Get-SegmentedPath) -join " $([PoshCode.Pansies.Entities]::ExtendedCharacters["Separator"]) ").Replace("$env:HOMEDRIVE\"," /") } -ForegroundColor White -BackgroundColor 'DarkBlue'},
-			{ if((Get-Acl (Get-Location).Path).AreAccessRulesProtected -eq $True) {New-PromptText { " $([PoshCode.Pansies.Entities]::ExtendedCharacters["Lock"]) " } -BackgroundColor Red -ForegroundColor White } },
-			{ New-PromptText { Get-GitStatusPowerline } -BackgroundColor DarkGray -ForegroundColor White }
-		)
+	[PoshCode.Pansies.Entities]::ExtendedCharacters.Separator = $([char]0x2572)	
+	[System.Collections.Generic.List[ScriptBlock]]$global:Prompt = @(
+		{New-PromptText {Get-Elapsed -Format "{0:mm\m\:ss\s\.ffff}ms" } -ErrorBackgroundColor DarkRed -ErrorForegroundColor White -ForegroundColor White -BackgroundColor DarkGray  },
+		{New-PromptText {Get-Date -f "HH:mm:ss"} -ForegroundColor Black -BackgroundColor Gray  },
+		{New-PromptText { $($MyInvocation.HistoryId) } -ForegroundColor White -BackgroundColor 'DarkGray'},
+		#{New-PromptText { $($MyInvocation.HistoryId) } -ForegroundColor White -BackgroundColor 'DarkBlue'},
+		{ if ($Global:IsAdmin) {New-PromptText {"wurzel"} -BackgroundColor DarkRed -ForegroundColor LightGray }else {New-PromptText { [environment]::UserName } -BackgroundColor Gray -ForegroundColor Black} },
+		{New-PromptText { "$($Env:COMPUTERNAME.ToLower())" } -ForegroundColor DarkGray -BackgroundColor Yellow},
+		{New-PromptText { ((Get-SegmentedPath) -join " $([PoshCode.Pansies.Entities]::ExtendedCharacters["Separator"]) ").Replace("$env:HOMEDRIVE\"," /") } -ForegroundColor White -BackgroundColor 'DarkBlue'},
+		{ if((Get-Acl (Get-Location).Path).AreAccessRulesProtected -eq $True) {New-PromptText { " $([PoshCode.Pansies.Entities]::ExtendedCharacters["Lock"]) " } -BackgroundColor 'DarkBlue' -ForegroundColor White } }
+		{ if(Get-Module psgit -ListAvailable) {New-PromptText { Get-GitStatusPowerline } -BackgroundColor DarkGray -ForegroundColor White}  }
+	)
 	
-		$Script:PowerLinePrompt = $global:Prompt
-		Set-PowerLinePrompt -SetCurrentDirectory -RestoreVirtualTerminal -FullColor -Prompt $global:Prompt -PowerLineFont -Title { 
-			$title ="PS PID: {0}" -f $PID
-			if(Test-Elevation){
-				"Admin - $title"
-			} else {
-				$title
-			}
-		} -Colors DarkRed, Black, DarkGray, DarkBlue, Cyan, Yellow, DarkBlue
+	$Script:PowerLinePrompt = $global:Prompt
+	Set-PowerLinePrompt -SetCurrentDirectory -RestoreVirtualTerminal -FullColor -Prompt $global:Prompt -PowerLineFont -Title { 
+		$title ="PS PID: {0}" -f $PID
+		if(Test-Elevation){
+			"Admin - $title"
+		} else {
+			$title
+		}
+	} -Colors DarkRed, Black, DarkGray, DarkBlue, Cyan, Yellow, DarkBlue
 }
 
 $env:PYTHONIOENCODING = "UTF-8"
